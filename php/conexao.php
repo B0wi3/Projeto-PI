a937ab043fac0dce7d568e08d527d777191adb26
Fernando Lopes <50033985+finandolopes@users.noreply.github.com>
2024-03-26 21:13:49 -0300

    Delete php/conexao.php

--- php/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('HOST','localhost');
-    define('USUARIO', 'root');
-    define('SENHA','');
-    define('DB','confinter');
-
-    $con = mysqli_connect(HOST,USUARIO,SENHA,DB) or die ('ERRO');
-
-?>
